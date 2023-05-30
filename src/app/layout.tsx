5dee33677a2f458658acf8882d5f2c64e4169a19
Minu Kim <minumoseskim@gmail.com>
2023-05-29 21:33:23 -0400

    add basic font and color styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,34 @@
 import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import clsx from 'clsx';
+import '~/styles/globals.css';
 
 export const metadata = {
   title: 'nextjs-template',
   description: 'nextjs-template',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="dark">
+      <body
+        className={clsx(
+          'min-h-screen bg-background font-sans text-foreground antialiased',
+          inter.className
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <main className="h-full p-6">
+      <h1 className="text-4xl font-medium">Display</h1>
+      <h2 className="text-2xl font-semibold">Title Large</h2>
+      <h3 className="text-xl font-semibold">Title Medium</h3>
+      <h3 className="text-base font-semibold">Title Small</h3>
+      <h3 className="text-xl font-normal">Subtitle</h3>
+      <p className="text-base">Body large</p>
+      <p className="text-sm">Body medium</p>
+      <p className="text-xs">Body small</p>
+    </main>
+  );
 }
