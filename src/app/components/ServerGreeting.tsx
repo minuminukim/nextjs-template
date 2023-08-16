c8184932b91401edef45cf3e64a3e13a4110ac83
Minu Kim <minumoseskim@gmail.com>
2023-08-16 19:19:48 -0400

    Remove TRPC

--- src/app/components/ServerGreeting.tsx
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { api } from '~/trpc/server';
-
-export async function ServerGreeting() {
-  const greeting = await api.greeting.query({ text: 'from server' });
-
-  return <>{greeting}</>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import { Suspense } from 'react';
 import { ThemeSwitch } from '~/ui/ThemeSwitch';
-import { ClientGreeting } from './components/ClientGreeting';
-import { ServerGreeting } from './components/ServerGreeting';
-import { api } from '~/trpc/server';
 
 export default async function Page() {
   const promise = new Promise<string>(async (resolve) => {
     await new Promise((r) => setTimeout(r, 1000)); // wait for demo purposes
-    resolve(api.greeting.query({ text: 'streamed server data' }));
+    resolve('streamed server data');
   });
 
   return (
@@ -16,23 +13,9 @@ export default async function Page() {
       <div className="my-4 ">
         <h1 className="text-4xl">Hello, next.js</h1>
       </div>
-      <>
-        <div>
-          <Suspense fallback={<>Loading client...</>}>
-            <ClientGreeting />
-          </Suspense>
-        </div>
-        <div>
-          <Suspense fallback={<>Loading server...</>}>
-            <ServerGreeting />
-          </Suspense>
-        </div>
-        <div>
-          <Suspense fallback={<>Loading stream...</>}>
-            <StreamedSC promise={promise} />
-          </Suspense>
-        </div>
-      </>
+      <Suspense fallback={<>Loading stream...</>}>
+        <StreamedSC promise={promise} />
+      </Suspense>
     </main>
   );
 }
